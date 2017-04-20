8ff15e92409825db5895f768f992ddb239da3d95
zipperr <zipperr@i.softbank.jp>
2017-04-20 15:30:09 +0900

    Cygwin対応

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .bashrc .bash_history .zshrc .zsh_history .irssi screenfetch-dev)
+DOT_FILES=(.vimrc .bashrc .bash_history .zshrc .zsh_history .minttyrc .irssi screenfetch-dev)
 for file in ${DOT_FILES[@]}
 do
 ln -s $HOME/dotfiles/$file $HOME/$file
