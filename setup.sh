1722e5f774c69db20071499840e5b86da25eee1c
zipperr <zipperr@i.softbank.jp>
2017-06-06 19:58:52 +0900

    git

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DOT_FILES=(.vimrc .bashrc .bash_profile .inputrc .bash_history .git-prompt.sh .gitconfig .zshrc .zsh_history .minttyrc .irssi)
+DOT_FILES=(.vimrc .bashrc .bash_profile .inputrc .bash_history .git-prompt.sh .gitconfig .gitignore_global .zshrc .zsh_history .minttyrc .irssi)
 for file in ${DOT_FILES[@]}
 do
 ln -s $HOME/dotfiles/$file $HOME/$file
