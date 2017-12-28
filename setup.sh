2e5a128b198723d142d7c4f1c0b8a6d827617754
zipperr <zipperr@i.softbank.jp>
2017-12-28 22:58:50 +0900

    a

--- setup.sh
+++ setup.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-if [ ! -d ~/.zsh ]; then ln -s ~/dotfiles/.zsh/ ~/.zsh > /dev/null 2>&1; fi
-
-DOT_FILES=(.vimrc .bashrc .zshrc .git-prompt.sh .gitconfig .gitignore_global )
+DOT_FILES=(.vimrc .bashrc .zshrc .git-prompt.sh .gitconfig .gitignore_global)
 for file in ${DOT_FILES[@]}
 do
 ln -s $HOME/dotfiles/$file $HOME/$file
