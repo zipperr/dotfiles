c322458fd7d8d58e3e0b0ac6bbefe686b2b66ef1
zipperr <zipperr@i.softbank.jp>
2018-01-11 22:45:23 +0900

    a

--- setup.bat
+++ setup.bat
@@ -9,5 +9,5 @@ XCOPY %HOMEPATH%"\dotfiles\.vim\colors\hybrid.vim" %HOMEPATH%"\.vim\colors\" /D 
 XCOPY %HOMEPATH%"\dotfiles\.vim\snippets" %HOMEPATH%"\.vim\snippets" /D /S /R /I /K /Y
 XCOPY %HOMEPATH%"\dotfiles\.vim\templates" %HOMEPATH%"\.vim\templates" /D /S /R /I /K /Y
 XCOPY %HOMEPATH%"\dotfiles\.vim\dll\vimproc_cygwin.dll" %HOMEPATH%"\.vim\dein\repos\github.com\Shougo\vimproc.vim\lib\" /D /S /R /I /K /Y
-MKDIR %HOMEPATH%"\.vim\tmp
+XCOPY %HOMEPATH%"\dotfiles\.vim\tmp\yankring_history_v2.txt" %HOMEPATH%"\.vim\tmp\" /D /S /R /I /K /Y
 exit 0
