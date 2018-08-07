33a2a9da4230d3d216f4b6779ad2302626111e6f
zipperr <zipperr@i.softbank.jp>
2018-08-08 00:21:13 +0900

    Update

--- pythonSetup.sh
+++ pythonSetup.sh
@@ -2,9 +2,6 @@
 
 if [ "$(uname)" == "Darwin" ]; then
     if [ `which pyenv` ]; then
-    echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-    echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-    echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
     pyenv install -v 3.7.0
     pyenv global 3.7.0
     pip install --upgrade pip
@@ -15,9 +12,6 @@ elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
 elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
     if [ `which git` ]; then
         git clone https://github.com/pyenv/pyenv.git ~/.pyenv
-        echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
-        echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
-        echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bashrc
         sudo apt-get -y install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev
         pyenv install -v 3.7.0
         pyenv global 3.7.0
