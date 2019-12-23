41e0b58bdd8a513a63969d9234d6d4f02b10a72e
zipperr <zipperr@i.softbank.jp>
2019-12-23 14:46:57 +0900

    Update

--- setup.sh
+++ setup.sh
@@ -94,87 +94,55 @@ setup(){
 }
 
 install_python(){
-    if [ "$(uname)" == "Darwin" ]; then
-        if [ `which pyenv` ]; then
-            while true;do
-                echo "Start python3.7.0 install"
-                echo "Type 'y' or 'n'"
-                read answer
-                case $answer in
-                    y)
+    while true;do
+        echo "Start python3.7.0 install"
+        echo "Type 'y' or 'n'"
+        read answer
+        case $answer in
+            y)
+                if [ "$(uname)" == "Darwin" ]; then
+                    if [ `which pyenv` ]; then
                         pyenv install -v 3.7.0
                         pyenv global 3.7.0
-                        break
-                        ;;
-                    n)
-                        break
-                        ;;
-                    *)
-                        echo -e "cannot understand $answer.\n"
-                        ;;
-                esac
-            done
-        fi
-        if [ `which pip` ]; then
-            pip install --upgrade pip
-            pip install numpy
-            pip install Flask
-            pip install vim-vint
-            pip install flask-socketIO
-        fi
-    elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
-        while true;do
-            echo "Start python3 install"
-            echo "Type 'y' or 'n'"
-            read answer
-            case $answer in
-                y)
+                    fi
+                    if [ `which pip` ]; then
+                        pip install --upgrade pip
+                        pip install numpy
+                        pip install Flask
+                        pip install vim-vint
+                        pip install flask-socketIO
+                    fi
+                elif [ "$(expr substr $(uname -s) 1 5)" == "MINGW" ]; then
                     echo "Go https://www.python.org/"
-                    break
-                    ;;
-                n)
-                    break
-                    ;;
-                *)
-                    echo -e "cannot understand $answer.\n"
-                    ;;
-            esac
-        done
-    elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-        if [ `which git` ]; then
-            while true;do
-                echo "Start python3.7.0 install"
-                echo "Type 'y' or 'n'"
-                read answer
-                case $answer in
-                    y)
+                elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+                    if [ `which git` ]; then
                         git clone https://github.com/pyenv/pyenv.git ~/.pyenv
                         sudo apt-get -y install git gcc make openssl libssl-dev libbz2-dev libreadline-dev libsqlite3-dev libffi-dev
                         pyenv install -v 3.7.0
                         pyenv global 3.7.0
-                        break
-                        ;;
-                    n)
-                        break
-                        ;;
-                    *)
-                        echo -e "cannot understand $answer.\n"
-                        ;;
-                esac
-            done
-        fi
-        if [ `which pip` ]; then
-            pip install --upgrade pip
-            pip install numpy
-            pip install Flask
-            pip install vim-vint
-            pip install flask-socketIO
-            pip install Flask-MySQL
-            pip install picamera
-            pip install --upgrade setuptools
-            pip install flask-bootstrap
-        fi
-    fi
+                    fi
+                    if [ `which pip` ]; then
+                        pip install --upgrade pip
+                        pip install numpy
+                        pip install Flask
+                        pip install vim-vint
+                        pip install flask-socketIO
+                        pip install Flask-MySQL
+                        pip install picamera
+                        pip install --upgrade setuptools
+                        pip install flask-bootstrap
+                    fi
+                fi
+                break
+                ;;
+            n)
+                break
+                ;;
+            *)
+                echo -e "cannot understand $answer.\n"
+                ;;
+        esac
+    done
 }
 
 echo "Start Setup"
